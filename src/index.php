a4fee0ca6597141b4eb4a337cdff62a316025cde
Philip Clifton <p.clifton@squawk1200.net>
2019-01-31 07:27:30 -0600

    Flesh out homepage post styles

--- src/index.php
+++ src/index.php
@@ -1,22 +1,29 @@
 <?php get_header(); ?>
 
-<div class="large-12 cell">
-	<h1>Posts</h1>
-</div>
+<div class="large-8 medium-8 cell">	
+	<?php if (have_posts()): ?>
+		<?php while (have_posts()): the_post(); ?>
+			<!-- TODO: should this become a template part so we can reuse it for single-post pages? -->
+			<div class="post-container">
+				<h3 class="post-title"><a href="<?=esc_url( get_permalink()) ?>"><?php the_title(); ?></a></h3>
+				<div class="post-meta">
+					<p><?=get_the_date() ?> &middot; <?=get_the_category_list( ',' ) ?> &middot; 1.5 hours</p>
+					<!-- @TODO actual hours eg c2c_get_custom('hours') when plugin is done -->
+				</div>
+				<div class="post-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+	
+	
+	<!-- <div class="primary callout">more content</div>
 
-<div class="large-8 medium-8 cell">
-	<h2>This is the main content area</h2>
-	
-	<div class="primary callout">content</div>
-	
-	<div class="primary callout">more content</div>
-	
-	<div class="primary callout">yet more content</div>
+	<div class="primary callout">yet more content</div> -->
 </div>
 
 <div class="large-4 medium-4 cell">
-	<h3>This is the secondary content area</h3>
-	
 	<div class="primary callout">content</div>
 	
 	<div class="primary callout">more content</div>
